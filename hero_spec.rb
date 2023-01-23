45b957cd5b3d6821a3e54065be9fc003a6af0ea1
Maksym Nenashev <57881489+maks-nenashev@users.noreply.github.com>
2023-01-23 11:30:16 +0100

    dorabotan "my first test"

--- hero_spec.rb
+++ hero_spec.rb
@@ -1,31 +1,25 @@
 require './hero'
 
     describe Hero do
-             
+         
+        before do
+          @hero = Hero.new 'mike'
+        end
              # uslowie testa
         it "has a capitalized name" do
-          hero = Hero.new 'mike'
-      
-          expect(hero.name).to eq 'Mike' # hero.name == "Mike"   :to samoe
+           #hero = Hero.new 'mike'       :S metodom "before" eto nenuzno! no dobowlaem "@"
+           expect(@hero.name).to eq 'Mike'   # hero.name == "Mike"   :to samoe
         end
              
-            # uslowie testa
         it "can power up" do
-          hero = Hero.new 'Mike'
-        
-          expect(hero.power_up).to eq 110   # hero.power_up == "110"  : to samoe
+           expect(@hero.power_up).to eq 110   # hero.power_up == "110"  : to samoe
         end
         
-        
         it "can power down" do
-          hero = Hero.new 'mike'
-
-          expect(hero.power_down).to eq 90
+            expect(@hero.power_down).to eq 90
         end
         
         it "displays full hero_info" do
-          hero = Hero.new 'mike'
-
-          expect(hero.hero_info).to eq "Mike has 100 health"
+            expect(@hero.hero_info).to eq "Mike has 100 health"
         end
  end
